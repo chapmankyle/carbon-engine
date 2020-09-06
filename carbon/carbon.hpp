bfaf03dabaf17d2119644308611d576e9e40b4ed
Kyle <kyleichapman@gmail.com>
2020-09-06 19:03:39 +0200

    Add header file to include all current structs

---
+++ carbon/carbon.hpp
@@ -0,0 +1,11 @@
+#pragma once
+
+#ifndef CARBON_HPP
+#define CARBON_HPP
+
+#include "common/debug_messenger.hpp"
+#include "common/utils.hpp"
+
+#include "core/instance.hpp"
+
+#endif // !CARBON_HPP
