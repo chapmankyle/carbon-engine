99e747d659885e45c6b9bc42aaad4e9adee2aec6
Kyle <kyleichapman@gmail.com>
2020-09-07 17:00:06 +0200

    Add header to check template types

---
+++ carbon/common/template_types.hpp
@@ -0,0 +1,34 @@
+#pragma once
+
+#ifndef COMMON_TEMPLATE_TYPES_HPP
+#define COMMON_TEMPLATE_TYPES_HPP
+
+#include <type_traits>
+
+#include "carbon/setup.hpp"
+
+namespace carbon {
+
+	namespace types {
+
+		template<class T>
+		struct is_supported_properties_struct {
+			static const bool value = false;
+		};
+
+		template<>
+		struct is_supported_properties_struct<VkExtensionProperties> {
+			static const bool value = true;
+		};
+
+		template<>
+		struct is_supported_properties_struct<VkLayerProperties> {
+			static const bool value = true;
+		};
+
+	} // namespace types
+
+} // namespace carbon
+
+#endif // !COMMON_TEMPLATE_TYPES_HPP
+
