0695aff37d05707a1435ba2c5e76cb5af7822586
Kyle <kyleichapman@gmail.com>
2020-09-07 14:00:59 +0200

    Use platform.hpp to enable and disable certain features based on compatibility

--- carbon/setup.hpp
+++ carbon/setup.hpp
@@ -10,13 +10,13 @@
 #define GLFW_INCLUDE_VULKAN
 #include <GLFW/glfw3.h>
 
-#include <memory>
+#include "platform.hpp"
 
 // ----------------------
 // -- USEFUL CONSTANTS --
 // ----------------------
 
-#define CARBON_ENABLE 1
+#define CARBON_ENABLE  1
 #define CARBON_DISABLE 0
 
 #define CARBON_VERSION_MAJOR 1
@@ -24,7 +24,7 @@
 #define CARBON_VERSION_PATCH 0
 
 #define CARBON_VERSION VK_MAKE_VERSION(CARBON_VERSION_MAJOR, CARBON_VERSION_MINOR, CARBON_VERSION_PATCH);
-#define CARBON_VERSION_STRING "Carbon Engine v1.0.0"
+#define CARBON_VERSION_MESSAGE "Carbon Engine v1.0.0"
 
 #define CARBON_ENGINE_NAME "Carbon Engine"
 
@@ -33,11 +33,105 @@
 // -----------
 
 #ifndef CARBON_DISABLE_DEBUG
-#include <iostream>
-#define CARBON_ENABLE_VALIDATION_LAYERS CARBON_ENABLE
+#	include <iostream>
+#	define CARBON_ENABLE_VALIDATION_LAYERS CARBON_ENABLE
 #else
-#define CARBON_ENABLE_VALIDATION_LAYERS CARBON_DISABLE
+#	define CARBON_ENABLE_VALIDATION_LAYERS CARBON_DISABLE
 #endif // CARBON_DISABLE_DEBUG
+
+// ------------------
+// -- C++ Standard --
+// ------------------
+
+#define CARBON_LANG_CXX0X_FLAG  (1 << 1)
+#define CARBON_LANG_CXX11_FLAG  (1 << 2)
+#define CARBON_LANG_CXX14_FLAG  (1 << 3)
+#define CARBON_LANG_CXX17_FLAG  (1 << 4)
+#define CARBON_LANG_CXX2A_FLAG  (1 << 5)
+
+#define CARBON_LANG_CXX0X  CARBON_LANG_CXX0X_FLAG
+#define CARBON_LANG_CXX11  (CARBON_LANG_CXX0X | CARBON_LANG_CXX11_FLAG)
+#define CARBON_LANG_CXX14  (CARBON_LANG_CXX11 | CARBON_LANG_CXX14_FLAG)
+#define CARBON_LANG_CXX17  (CARBON_LANG_CXX14 | CARBON_LANG_CXX17_FLAG)
+#define CARBON_LANG_CXX2A  (CARBON_LANG_CXX17 | CARBON_LANG_CXX2A_FLAG)
+
+// Controls language extensions
+#define CARBON_LANG_EXT CARBON_DISABLE
+
+// check if using Visual Studio compiler
+#if CARBON_COMPILER & CARBON_COMPILER_VC && defined(_MSVC_LANG)
+#	if CARBON_COMPILER >= CARBON_COMPILER_VC15_7
+#		define CARBON_LANG_PLATFORM _MSVC_LANG
+#	elif CARBON_COMPILER >= CARBON_COMPILER_VC15
+#		if _MSVC_LANG > 201402L
+#			define CARBON_LANG_PLATFORM 201402L
+#		else
+#			define CARBON_LANG_PLATFORM _MSVC_LANG
+#		endif
+#	else
+#		define CARBON_LANG_PLATFORM 0
+#	endif
+#else
+#	define CARBON_LANG_PLATFORM 0
+#endif
+
+// check C++ standard
+#if __cplusplus > 201703L || CARBON_LANG_PLATFORM > 201703L
+#	define CARBON_LANG (CARBON_LANG_CXX2A | CARBON_LANG_EXT)
+#elif __cplusplus == 201703L || CARBON_LANG_PLATFORM == 201703L
+#	define CARBON_LANG (CARBON_LANG_CXX17 | CARBON_LANG_EXT)
+#elif __cplusplus == 201402L || __cplusplus == 201406L || __cplusplus == 201500L || CARBON_LANG_PLATFORM == 201402L
+#	define CARBON_LANG (CARBON_LANG_CXX14 | CARBON_LANG_EXT)
+#elif __cplusplus == 201103L || CARBON_LANG_PLATFORM == 201103L
+#	define CARBON_LANG (CARBON_LANG_CXX11 | CARBON_LANG_EXT)
+#else
+#	define CARBON_LANG (0 | CARBON_LANG_EXT)
+#endif
+
+// ------------------
+// -- C++ Features --
+// ------------------
+
+// -- constexpr
+// N2235 Generalized Constant Expressions http://www.open-std.org/JTC1/SC22/WG21/docs/papers/2007/n2235.pdf
+// N3652 Relaxing constraints on constexpr functions http://www.open-std.org/JTC1/SC22/WG21/docs/papers/2013/n3652.html
+#if (CARBON_COMPILER & CARBON_COMPILER_CLANG)
+#	define CARBON_HAS_CONSTEXPR __has_feature(cxx_relaxed_constexpr)
+#elif (CARBON_LANG & CARBON_LANG_CXX14_FLAG)
+#	define CARBON_HAS_CONSTEXPR 1
+#else
+#	define CARBON_HAS_CONSTEXPR ((CARBON_LANG & CARBON_LANG_CXX0X_FLAG) && \
+		((CARBON_COMPILER & CARBON_COMPILER_VC) && (CARBON_COMPILER >= CARBON_COMPILER_VC15)))
+#endif
+
+#if CARBON_HAS_CONSTEXPR
+#	define CARBON_CONSTEXPR constexpr
+#else
+#	define CARBON_CONSTEXPR
+#endif
+
+// -- static_assert
+// N1720 Proposal to Add Static Assertions to the Core Language http://www.open-std.org/JTC1/SC22/WG21/docs/papers/2004/n1720.html
+#if CARBON_COMPILER & CARBON_COMPILER_CLANG
+#	define CARBON_HAS_STATIC_ASSERT __has_feature(cxx_static_assert)
+#elif CARBON_LANG & CARBON_LANG_CXX11_FLAG
+#	define CARBON_HAS_STATIC_ASSERT 1
+#else
+#	define CARBON_HAS_STATIC_ASSERT ((CARBON_LANG & CARBON_LANG_CXX0X_FLAG) && \
+		((CARBON_COMPILER & CARBON_COMPILER_VC)))
+#endif
+
+#if CARBON_HAS_STATIC_ASSERT
+#	define CARBON_STATIC_ASSERT(x, message) static_assert(x, message)
+#elif CARBON_COMPILER & CARBON_COMPILER_VC
+#	define CARBON_STATIC_ASSERT(x, message) typedef char __CASSERT__##__LINE__[(x) ? 1 : -1]
+#else
+#	define CARBON_STATIC_ASSERT(x, message) assert(x && message)
+#endif
+
+// -- inline
+
+#define CARBON_INLINE inline
 
 // ----------------------
 // -- USEFUL FUNCTIONS --
