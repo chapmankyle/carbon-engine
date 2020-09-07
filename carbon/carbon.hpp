7ffa3605a1b0559dc1b276fd95b8ec0bdd679888
Kyle <kyleichapman@gmail.com>
2020-09-07 14:15:49 +0200

    Include setup header

--- carbon/carbon.hpp
+++ carbon/carbon.hpp
@@ -3,9 +3,11 @@
 #ifndef CARBON_HPP
 #define CARBON_HPP
 
+#include "setup.hpp"
+
 #include "common/debug_messenger.hpp"
 #include "common/utils.hpp"
 
 #include "core/instance.hpp"
 
-#endif // !CARBON_HPP
+#endif // CARBON_HPP
