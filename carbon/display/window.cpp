a642ed89bf10604d262fd2e3fb352c711b948542
Kyle <kyleichapman@gmail.com>
2020-09-08 13:23:21 +0200

    Add window class to display a GLFW window

---
+++ carbon/display/window.cpp
@@ -0,0 +1,67 @@
+#include "window.hpp"
+
+void carbon::Window::init() {
+	if (!glfwInit()) {
+		throw std::runtime_error("Failed to initialize GLFW!");
+	}
+
+	// specify not to use OpenGL context
+	glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+
+	// no resize support, yet!
+	glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+
+	// create window
+	m_window = glfwCreateWindow(m_width, m_height, m_title, nullptr, nullptr);
+}
+
+
+carbon::Window::Window(const char *title, const int width, const int height) {
+	m_title = title;
+	m_width = width;
+	m_height = height;
+
+	init();
+}
+
+
+carbon::Window::Window() {
+	m_title = m_default_title;
+	m_width = m_default_width;
+	m_height = m_default_height;
+
+	init();
+}
+
+
+carbon::Window::~Window() {
+	destroy();
+}
+
+
+void carbon::Window::destroy() {
+	if (m_window == nullptr) {
+		return;
+	}
+
+	// destroy and free window
+	glfwDestroyWindow(m_window);
+
+	// terminate GLFW
+	glfwTerminate();
+}
+
+
+const char *carbon::Window::getTitle() {
+	return m_title;
+}
+
+
+const int carbon::Window::getWidth() {
+	return m_width;
+}
+
+
+const int carbon::Window::getHeight() {
+	return m_height;
+}
---
+++ carbon/display/window.hpp
@@ -0,0 +1,100 @@
+#pragma once
+
+#ifndef DISPLAY_WINDOW_HPP
+#define DISPLAY_WINDOW_HPP
+
+#include "carbon/setup.hpp"
+
+namespace carbon {
+
+	class Window {
+
+	private:
+
+		/*
+		 * @brief Handle to the GLFW window object.
+		 */
+		GLFWwindow *m_window;
+
+		/*
+		 * @brief Title of the window.
+		 */
+		const char *m_title;
+
+		/*
+		 * @brief Width of the window.
+		 */
+		int m_width;
+
+		/*
+		 * @brief Height of the window.
+		 */
+		int m_height;
+
+		/*
+		 * @brief Title to use when no title is supplied.
+		 */
+		const char *m_default_title = "Game";
+
+		/*
+		 * @brief Width to use when no width is supplied.
+		 */
+		const int m_default_width = 800;
+
+		/*
+		 * @brief Height to use when no height is supplied.
+		 */
+		const int m_default_height = 600;
+
+		/*
+		 * @brief Initializes the window.
+		 */
+		void init();
+
+	public:
+
+		/*
+		 * @brief Initializes a GLFW window for on-screen rendering.
+		 * @param title The title of the window.
+		 * @param width The width of the window.
+		 * @param height The height of the window.
+		 */
+		Window(const char *title, const int width, const int height);
+
+		/*
+		 * @brief Initializes a GLFW window with default title, width
+		 * and height.
+		 */
+		Window();
+
+		/*
+		 * @brief Destructor for the GLFW window.
+		 */
+		~Window();
+
+		/*
+		 * @brief Destroys the window object.
+		 */
+		void destroy();
+
+		/*
+		 * @returns The title of the window.
+		 */
+		const char *getTitle();
+
+		/*
+		 * @returns The width of the window.
+		 */
+		const int getWidth();
+
+		/*
+		 * @returns The height of the window.
+		 */
+		const int getHeight();
+
+	};
+
+} // namespace carbon
+
+#endif // DISPLAY_WINDOW_HPP
+
