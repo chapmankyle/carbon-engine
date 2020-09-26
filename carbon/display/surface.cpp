a946c01b0f032ad18b1fe024a31c47fc58fc9862
Kyle <kyleichapman@gmail.com>
2020-09-26 10:30:37 +0200

    Add surface class

---
+++ carbon/display/surface.cpp
@@ -0,0 +1,28 @@
+#include "surface.hpp"
+
+#include "carbon/core/instance.hpp"
+
+carbon::Surface::Surface(carbon::Instance *instance, GLFWwindow *window)
+	: m_instance(instance)
+	, m_window(window)
+{
+	// check if surface creation was successful
+	if (glfwCreateWindowSurface(m_instance->getHandle(), m_window, nullptr, &m_surface) != VK_SUCCESS) {
+		throw std::runtime_error("Failed to create window surface!");
+	}
+}
+
+
+carbon::Surface::~Surface() {
+	destroy();
+}
+
+
+void carbon::Surface::destroy() {
+	vkDestroySurfaceKHR(m_instance->getHandle(), m_surface, nullptr);
+}
+
+
+const VkSurfaceKHR& carbon::Surface::getHandle() const {
+	return m_surface;
+}
