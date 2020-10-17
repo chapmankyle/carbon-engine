39ae8a007fc37a09d01991d49bec6c87416c56d1
Kyle <kyleichapman@gmail.com>
2020-10-17 23:52:08 +0200

    Include all current files in carbon header

--- carbon/carbon.hpp
+++ carbon/carbon.hpp
@@ -10,8 +10,13 @@
 #include "common/utils.hpp"
 
 #include "core/instance.hpp"
+#include "core/logical_device.hpp"
 #include "core/physical_device.hpp"
 
+#include "display/surface.hpp"
+#include "display/swapchain.hpp"
 #include "display/window.hpp"
+
+#include "pipeline/render_pass.hpp"
 
 #endif // CARBON_HPP
