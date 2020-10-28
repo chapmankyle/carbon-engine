17aa5fda91ca4964e4b299f2839d6eaaeef7785f
Kyle <kyleichapman@gmail.com>
2020-10-28 16:35:14 +0200

    Move implementation for logical device into namespace

--- carbon/core/logical_device.cpp
+++ carbon/core/logical_device.cpp
@@ -1,154 +1,154 @@
 #include "logical_device.hpp"
 
 #include "instance.hpp"
-#include "carbon/display/surface.hpp"
 #include "physical_device.hpp"
+#include "carbon/display/surface.hpp"
 
-void carbon::LogicalDevice::findQueueFamilyIndices() {
-    uint32_t numQueueFamilies{ 0 };
+namespace carbon {
 
-    // get physical device queue family properties
-    VkPhysicalDevice physDevice{ m_physical_device->getHandle() };
-    vkGetPhysicalDeviceQueueFamilyProperties(physDevice, &numQueueFamilies, nullptr);
+	void LogicalDevice::findQueueFamilyIndices() {
+		u32 numQueueFamilies{ 0 };
 
-    // store queue families
-    std::vector<VkQueueFamilyProperties> queueFamilies(numQueueFamilies);
-    vkGetPhysicalDeviceQueueFamilyProperties(physDevice, &numQueueFamilies, queueFamilies.data());
+		// get physical device queue family properties
+		VkPhysicalDevice physDevice{ m_physical_device->getHandle() };
+		vkGetPhysicalDeviceQueueFamilyProperties(physDevice, &numQueueFamilies, nullptr);
 
-    int i{ 0 };
+		// store queue families
+		std::vector<VkQueueFamilyProperties> queueFamilies(numQueueFamilies);
+		vkGetPhysicalDeviceQueueFamilyProperties(physDevice, &numQueueFamilies, queueFamilies.data());
 
-    // find first index of queue family that supports commands
-    for (const auto &queueFam : queueFamilies) {
-        // check for graphics support
-        if (queueFam.queueFlags & VK_QUEUE_GRAPHICS_BIT) {
-            m_queue_family_indices.graphicsFamily = i;
-        }
+		u32 i{ 0 };
 
-        // check for surface support
-        VkBool32 presentSupport{ false };
-        vkGetPhysicalDeviceSurfaceSupportKHR(m_physical_device->getHandle(), i, m_surface->getHandle(), &presentSupport);
+		// find first index of queue family that supports commands
+		for (const auto &queueFam : queueFamilies) {
+			// check for graphics support
+			if (queueFam.queueFlags & VK_QUEUE_GRAPHICS_BIT) {
+				m_queue_family_indices.graphicsFamily = i;
+			}
 
-        if (queueFam.queueCount > 0 && presentSupport) {
-            m_queue_family_indices.presentFamily = i;
-        }
+			// check for surface support
+			VkBool32 presentSupport{ false };
+			vkGetPhysicalDeviceSurfaceSupportKHR(m_physical_device->getHandle(), i, m_surface->getHandle(), &presentSupport);
 
-        // check for compute support
-        if (queueFam.queueFlags & VK_QUEUE_COMPUTE_BIT) {
-            m_queue_family_indices.computeFamily = i;
-        }
+			if (queueFam.queueCount > 0 && presentSupport) {
+				m_queue_family_indices.presentFamily = i;
+			}
 
-        // check for transfer support
-        if (queueFam.queueFlags & VK_QUEUE_TRANSFER_BIT) {
-            m_queue_family_indices.transferFamily = i;
-        }
+			// check for compute support
+			if (queueFam.queueFlags & VK_QUEUE_COMPUTE_BIT) {
+				m_queue_family_indices.computeFamily = i;
+			}
 
-        // check if all families are supported
-        if (m_queue_family_indices.hasFamilies()) {
-            break;
-        }
+			// check for transfer support
+			if (queueFam.queueFlags & VK_QUEUE_TRANSFER_BIT) {
+				m_queue_family_indices.transferFamily = i;
+			}
 
-        i++;
-    }
+			// check if all families are supported
+			if (m_queue_family_indices.hasFamilies()) {
+				break;
+			}
 
-    if (m_queue_family_indices.graphicsFamily == UINT32_MAX) {
-        throw std::runtime_error("[ERROR] No graphics family support.");
-    }
-}
+			i++;
+		}
 
-
-void carbon::LogicalDevice::createDevice() {
-    // unique indices for queue families
-    std::set<uint32_t> uniqueQueueFamilies = {
-        m_queue_family_indices.graphicsFamily,
-        m_queue_family_indices.presentFamily
-    };
-
-    // createinfo for each queue family
-    std::vector<VkDeviceQueueCreateInfo> createInfoQueues(uniqueQueueFamilies.size());
-
-    // priority given to this queue
-    float queuePriority{ 1.0f };
-
-    for (const uint32_t queueFam : uniqueQueueFamilies) {
-        VkDeviceQueueCreateInfo queueCreateInfo;
-        carbon::initStruct(queueCreateInfo, VK_STRUCTURE_TYPE_DEVICE_QUEUE_CREATE_INFO);
-
-        queueCreateInfo.queueFamilyIndex = queueFam;
-        queueCreateInfo.queueCount = 1;
-        queueCreateInfo.pQueuePriorities = &queuePriority;
-        createInfoQueues.push_back(queueCreateInfo);
-    }
-
-    // specify device features to use
-    VkPhysicalDeviceFeatures deviceFeats{};
-    deviceFeats.samplerAnisotropy = VK_TRUE;
-    deviceFeats.sampleRateShading = VK_TRUE;
-
-    std::vector<const char *> deviceExtensions{ m_physical_device->getDeviceExtensions() };
-    std::vector<const char *> validationLayers{ m_instance->getEnabledValidationLayers() };
-
-    // start creating device information
-    VkDeviceCreateInfo createInfo;
-    carbon::initStruct(createInfo, VK_STRUCTURE_TYPE_DEVICE_CREATE_INFO);
-
-    createInfo.queueCreateInfoCount = static_cast<uint32_t>(createInfoQueues.size());
-    createInfo.pQueueCreateInfos = createInfoQueues.data();
-
-    createInfo.pEnabledFeatures = &deviceFeats;
-
-    createInfo.enabledExtensionCount = static_cast<uint32_t>(deviceExtensions.size());
-    createInfo.ppEnabledExtensionNames = deviceExtensions.data();
-
-    if (m_instance->isValidationEnabled()) {
-        createInfo.enabledLayerCount = static_cast<uint32_t>(validationLayers.size());
-        createInfo.ppEnabledLayerNames = validationLayers.data();
-    } else {
-        createInfo.enabledLayerCount = 0;
-        createInfo.ppEnabledLayerNames = nullptr;
-    }
-
-    // attempt to create logical device
-    if (vkCreateDevice(m_physical_device->getHandle(), &createInfo, nullptr, &m_device) != VK_SUCCESS) {
-        throw std::runtime_error("[ERROR] Failed to create logical device!");
-    }
-
-    // create single graphics queue from device
-    vkGetDeviceQueue(m_device, m_queue_family_indices.graphicsFamily, 0, &m_graphics_queue);
-    vkGetDeviceQueue(m_device, m_queue_family_indices.presentFamily, 0, &m_present_queue);
-}
-
-carbon::LogicalDevice::LogicalDevice(
-    carbon::Instance *instance,
-    carbon::PhysicalDevice *physicalDevice,
-    carbon::Surface *surface
-)
-    : m_instance(instance)
-    , m_physical_device(physicalDevice)
-    , m_surface(surface)
-{
-    // find queue families and then create the logical device
-    findQueueFamilyIndices();
-    createDevice();
-}
+		if (m_queue_family_indices.graphicsFamily == u32_max) {
+			throw std::runtime_error("[ERROR] No graphics family support.");
+		}
+	}
 
 
-carbon::LogicalDevice::~LogicalDevice() {
-    destroy();
-}
+	void LogicalDevice::createDevice() {
+		// unique indices for queue families
+		std::set<u32> uniqueQueueFamilies = {
+			m_queue_family_indices.graphicsFamily,
+			m_queue_family_indices.presentFamily
+		};
+
+		// createinfo for each queue family
+		std::vector<VkDeviceQueueCreateInfo> createInfoQueues(uniqueQueueFamilies.size());
+
+		// priority given to this queue
+		float queuePriority{ 1.0f };
+
+		for (const u32 queueFam : uniqueQueueFamilies) {
+			VkDeviceQueueCreateInfo queueCreateInfo;
+			initStruct(queueCreateInfo, VK_STRUCTURE_TYPE_DEVICE_QUEUE_CREATE_INFO);
+
+			queueCreateInfo.queueFamilyIndex = queueFam;
+			queueCreateInfo.queueCount = 1;
+			queueCreateInfo.pQueuePriorities = &queuePriority;
+			createInfoQueues.push_back(queueCreateInfo);
+		}
+
+		// specify device features to use
+		VkPhysicalDeviceFeatures deviceFeats{};
+		deviceFeats.samplerAnisotropy = VK_TRUE;
+		deviceFeats.sampleRateShading = VK_TRUE;
+
+		std::vector<const char *> deviceExtensions{ m_physical_device->getDeviceExtensions() };
+		std::vector<const char *> validationLayers{ m_instance->getEnabledValidationLayers() };
+
+		// start creating device information
+		VkDeviceCreateInfo createInfo;
+		initStruct(createInfo, VK_STRUCTURE_TYPE_DEVICE_CREATE_INFO);
+
+		createInfo.queueCreateInfoCount = static_cast<u32>(createInfoQueues.size());
+		createInfo.pQueueCreateInfos = createInfoQueues.data();
+
+		createInfo.pEnabledFeatures = &deviceFeats;
+
+		createInfo.enabledExtensionCount = static_cast<u32>(deviceExtensions.size());
+		createInfo.ppEnabledExtensionNames = deviceExtensions.data();
+
+		if (m_instance->isValidationEnabled()) {
+			createInfo.enabledLayerCount = static_cast<u32>(validationLayers.size());
+			createInfo.ppEnabledLayerNames = validationLayers.data();
+		} else {
+			createInfo.enabledLayerCount = 0;
+			createInfo.ppEnabledLayerNames = nullptr;
+		}
+
+		// attempt to create logical device
+		if (vkCreateDevice(m_physical_device->getHandle(), &createInfo, nullptr, &m_device) != VK_SUCCESS) {
+			throw std::runtime_error("[ERROR] Failed to create logical device!");
+		}
+
+		// create single graphics queue from device
+		vkGetDeviceQueue(m_device, m_queue_family_indices.graphicsFamily, 0, &m_graphics_queue);
+		vkGetDeviceQueue(m_device, m_queue_family_indices.presentFamily, 0, &m_present_queue);
+	}
+
+	LogicalDevice::LogicalDevice(Instance *instance, PhysicalDevice *physicalDevice, Surface *surface)
+		: m_instance(instance)
+		, m_physical_device(physicalDevice)
+		, m_surface(surface)
+	{
+		// find queue families and then create the logical device
+		findQueueFamilyIndices();
+		createDevice();
+	}
 
 
-void carbon::LogicalDevice::destroy() {
-    if (m_device == VK_NULL_HANDLE) {
-        return;
-    }
+	LogicalDevice::~LogicalDevice() {
+		destroy();
+	}
 
-    // wait for asynchronous drawing commands to complete
-    vkDeviceWaitIdle(m_device);
 
-    // destroy and reset
-    vkDestroyDevice(m_device, nullptr);
-    m_device = VK_NULL_HANDLE;
+	void LogicalDevice::destroy() {
+		if (m_device == VK_NULL_HANDLE) {
+			return;
+		}
 
-    // m_surface is member class and thus will be destroyed
-    // after this class destructor
-}
+		// wait for asynchronous drawing commands to complete
+		vkDeviceWaitIdle(m_device);
+
+		// destroy and reset
+		vkDestroyDevice(m_device, nullptr);
+		m_device = VK_NULL_HANDLE;
+
+		// m_surface is member class and thus will be destroyed
+		// after this class destructor
+	}
+
+} // namespace carbon
