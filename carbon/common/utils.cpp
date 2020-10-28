4dffc27f4f55d4072ebf71a64f0672e76d00d076
Kyle <kyleichapman@gmail.com>
2020-10-28 12:18:14 +0200

    Move implementation for utils into relevant namespaces

--- carbon/common/utils.cpp
+++ carbon/common/utils.cpp
@@ -1,37 +1,46 @@
 #include "utils.hpp"
 
-const std::vector<VkLayerProperties> carbon::utils::getSupportedValidationLayers() {
-	uint32_t numLayers{ 0 };
+namespace carbon {
 
-	// request number of layers
-	vkEnumerateInstanceLayerProperties(&numLayers, nullptr);
+	namespace utils {
 
-	// put supported layers into std::vector
-	std::vector<VkLayerProperties> supportedLayers(numLayers);
-	vkEnumerateInstanceLayerProperties(&numLayers, supportedLayers.data());
+		const std::vector<VkLayerProperties> getSupportedValidationLayers() {
+			u32 numLayers{ 0 };
 
-	return supportedLayers;
-}
+			// request number of layers
+			vkEnumerateInstanceLayerProperties(&numLayers, nullptr);
 
+			// put supported layers into std::vector
+			std::vector<VkLayerProperties> supportedLayers(numLayers);
+			vkEnumerateInstanceLayerProperties(&numLayers, supportedLayers.data());
 
-const std::vector<VkExtensionProperties> carbon::utils::getSupportedExtensions() {
-	uint32_t numExtensions{ 0 };
-
-	// request number of extensions available
-	vkEnumerateInstanceExtensionProperties(nullptr, &numExtensions, nullptr);
-
-	// pack available extensions into vector
-	std::vector<VkExtensionProperties> supportedExtensions(numExtensions);
-	vkEnumerateInstanceExtensionProperties(nullptr, &numExtensions, supportedExtensions.data());
-
-	return supportedExtensions;
-}
+			return supportedLayers;
+		}
 
 
-int32_t carbon::utils::compare(const char *str, const VkExtensionProperties &propStruct) {
-	return strcmp(str, propStruct.extensionName);
-}
+		const std::vector<VkExtensionProperties> getSupportedExtensions() {
+			u32 numExtensions{ 0 };
 
-int32_t carbon::utils::compare(const char *str, const VkLayerProperties &propStruct) {
-	return strcmp(str, propStruct.layerName);
-}
+			// request number of extensions available
+			vkEnumerateInstanceExtensionProperties(nullptr, &numExtensions, nullptr);
+
+			// pack available extensions into vector
+			std::vector<VkExtensionProperties> supportedExtensions(numExtensions);
+			vkEnumerateInstanceExtensionProperties(nullptr, &numExtensions, supportedExtensions.data());
+
+			return supportedExtensions;
+		}
+
+
+		i32 compare(const char *str, const VkExtensionProperties &propStruct) {
+			return strcmp(str, propStruct.extensionName);
+		}
+
+
+		i32 compare(const char *str, const VkLayerProperties &propStruct) {
+			return strcmp(str, propStruct.layerName);
+		}
+
+	} // namespace utils
+
+} // namespace carbon
