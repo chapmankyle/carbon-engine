a95469810be880d70b15d09435e3b973a2f12f1a
Kyle <kyleichapman@gmail.com>
2020-11-04 14:43:56 +0200

    Show estimated aspect ratio in console

--- test/main.cpp
+++ test/main.cpp
@@ -19,11 +19,9 @@ int main() {
 
 	std::cout << "Validation ?= " << std::boolalpha << engine.getInstance().isValidationEnabled() << std::noboolalpha << '\n';
 	std::cout << "\nAspect ratio = " << engine.getWindow().getAspectRatio() << '\n';
+	std::cout << "Aspect ratio => " << carbon::utils::getEstimatedAspectRatio(properties.width, properties.height) << '\n';
 
-	carbon::i32 factor = carbon::utils::gcd(properties.width, properties.height);
-	std::cout << "Aspect ratio [factor=" << factor << "] => " << properties.width / factor << ':' << properties.height / factor << '\n';
-
-	std::cout << engine.getPhysicalDevice().getPropertiesAsStr() << '\n';
+	std::cout << '\n' << engine.getPhysicalDevice().getPropertiesAsStr() << '\n';
 
 	// main loop when window is open
 	while (engine.isRunning()) {
