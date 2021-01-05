dad81be4f5cb18bc480e9c1e6111e753aff1f41f
Kyle Chapman <kyleichapman@gmail.com>
2021-01-05 15:45:38 +0200

    Show root directory in console

--- test/main.cpp
+++ test/main.cpp
@@ -1,5 +1,7 @@
 #include "carbon/carbon.hpp"
 
+#include <iostream>
+
 int main() {
 
 	// to disable debug messages and validation layers, add
@@ -7,15 +9,23 @@ int main() {
 	// before the
 	// #include "carbon/carbon.hpp"
 
+	std::cout << "Root directory = |" << carbon::paths::ROOT_DIR << "|\n\n";
+
+	// setup window properties
 	carbon::window::Props properties;
 	properties.title = "Work In Progress: Game";
 	properties.version = carbon::utils::version{ 1, 1, 0 };
 	properties.width = 1280;
 	properties.height = 720;
 
+	// setup timer
 	carbon::Timer t;
+
+	// start timing
+	t.reset();
 	carbon::Engine engine(properties);
 	std::cout << "\nEngine creation took " << t.elapsed() << " ms\n";
+
 	std::cout << "Validation ?= " << std::boolalpha << engine.isValidationEnabled() << std::noboolalpha << '\n';
 
 	std::cout << "\nInitial position = " << carbon::utils::showVector(engine.getWindow().getPosition()) << '\n';
